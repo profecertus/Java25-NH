55be2ef9a7b8de1d8ca1c7dd438fb9cf0270333f
profecertus <84884709+profecertus@users.noreply.github.com>
2026-07-13 20:36:52 -0500

    Sesion07

---
+++ demo07/src/main/java/pe/nh/demo07/model/Producto.java
@@ -0,0 +1,20 @@
+package pe.nh.demo07.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Producto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private double precio;
+    private String categoria;
+}
